1dceac383d4b1abba84b77d3059561244b7f4972
ajaykumarj1988 <66014687+ajaykumarj1988@users.noreply.github.com>
2020-06-11 16:34:47 +0530

    Ajay Changes

--- src/test/java/com/ether/runner/CucumberTestRunner.java
+++ src/test/java/com/ether/runner/CucumberTestRunner.java
@@ -15,9 +15,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
         features = {"src/test/java/Feature_Files/"},
         glue = "com.ether.steps",
         dryRun=false,
-       tags = {"@karthik,@Priyanka,@Sugan_Samp,@Anjana,@Salma,@Ajay,@Vineeth,@Syed"}	
-    //tags = {"@karthik"}
-		//tags = {"@Sugan_Samp"}
+       tags = {",@Ajay"}	
         )
 
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
